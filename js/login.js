e7a323b49eb16c2dd24d183e86c7b6fa8107d823
Retesh-Gorai <retesh.retesh.gorai@gmail.com>
2020-06-07 14:05:35 +0530

    login js added

--- js/login.js
+++ js/login.js
@@ -1,55 +1,57 @@
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     console.log('DOM fully loaded and parsed');
-//     document.getElementById("loginbtn").addEventListener('click', onSubmitClick);
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    document.getElementById("loginBtn").addEventListener('click', onLoginBtnClick);
 
-//     function onSubmitClick() {
-//         // alert("hi");
-        
-//         var i_no = document.getElementById("i_no").value;
-//         var psw = document.getElementById("password").value;
+    function onLoginBtnClick() {
+        // alert("hi");
+        // var name = document.getElementById("name").value;
+        // var email = document.getElementById("email").value;
+        var uname = document.getElementById("uname").value;
+        var psw = document.getElementById("pswd").value;
 
-//         var jsonObject = {
-//             "i_no": i_no,
-//             "password": psw
-//         }
-//     // console.log(jsonObject);
-//         // console.log(jsonObject);
-//         // //TODO Pass this jsonObject to the ajax call
+        var jsonObject = {
+            // "firstName": name,
+            "username": uname,
+            // "lastName": role,
+            "password": psw
+        }
+        // console.log(jsonObject);
+        // console.log(jsonObject);
+        // //TODO Pass this jsonObject to the ajax call
 
-//         // const dummyData = {
-//         //     "name": "test",
-//         //     "salary": "123",
-//         //     "age": "23"
-//         // };
+        // const dummyData = {
+        //     "name": "test",
+        //     "salary": "123",
+        //     "age": "23"
+        // };
 
-//         const jsonString = JSON.stringify(jsonObject);
-//         // console.log(jsonString);
-//         const xhr = new XMLHttpRequest();
+        const jsonString = JSON.stringify(jsonObject);
+        // console.log(jsonString);
+        const xhr = new XMLHttpRequest();
 
-//         xhr.onreadystatechange = function () {
-//             if (xhr.readyState == XMLHttpRequest.DONE) {
-//                 // alert(xhr.responseText);
-//                 // console.log(xhr.responseText);
-//                 var datatext = xhr.responseText;
-//                 var jsonResponse = JSON.parse(datatext);
-//                 // console.log("Your id is "+jsonResponse.id);
-//                 var status = jsonResponse.status;
-//                 if (status == 200){
-//                     window.location.href = "homeDummy.html";
-//                 }
-//                 else
-//                     alert("Wrong Credentials")
-                
-//             }
-//         }
-//         xhr.open("POST", "http://localhost:8020/login");
-//         xhr.setRequestHeader("Content-Type", "application/json");
-//         xhr.setRequestHeader("Access-Control-Allow-Origin",'*');
-//         xhr.send(jsonString);
-
-
-//     }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                // alert(xhr.responseText);
+                // console.log(xhr.responseText);
+                var datatext = xhr.responseText;
+                var jsonResponse = JSON.parse(datatext);
+                // console.log("Your id is "+jsonResponse.id);
+                var status = jsonResponse.status;
+                if (status == 200){
+                    window.location.href = "loginSuccess.html";
+                }
+                else
+                    alert("Wrong Credentials")
+                // alert("Registration Successful! with response  "+jsonResponse.status );
+            }
+        }
+        xhr.open("POST", "http://localhost:8020/login");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Access-Control-Allow-Origin","*");
+        xhr.send(jsonString);
 
 
-// });
+    }
 
+
+});
