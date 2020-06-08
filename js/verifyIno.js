94c69aa1abdd08faa62d61f149f3cd98e7948a6f
Retesh-Gorai <retesh.retesh.gorai@gmail.com>
2020-06-08 15:52:37 +0530

    up

--- js/verifyIno.js
+++ js/verifyIno.js
@@ -15,20 +15,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
         const xhr = new XMLHttpRequest();
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
+        // xhr.onreadystatechange = function () {
+        //     if (xhr.readyState == XMLHttpRequest.DONE) {
 
-                var datatext = xhr.responseText;
-                // var jsonResponse = JSON.parse(datatext);
-                // var status = jsonResponse.status;
-                if (datatext === "ok"){
-                    alert("Verified");
-                }
-                else
-                    alert("Could not verify");
+        //         var datatext = xhr.responseText;
+        //         var jsonResponse = JSON.parse(datatext);
+        //         var status = jsonResponse.status;
+        //         if (datatext === "ok"){
+        //             alert("Verified");
+        //         }
+        //         else
+        //             alert("Could not verify");
 
-            }
-        }
+        //     }
+        // }
         xhr.open("POST", "http://localhost:8020/approve");
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.setRequestHeader("Access-Control-Allow-Origin",'*');
